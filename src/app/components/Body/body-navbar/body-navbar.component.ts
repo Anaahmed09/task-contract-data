3b2b10f2bb8d5b8afa54d072958e7031a329a781
Ahmed Ramadan <anaahmedca@gmail.com>
2023-06-08 14:41:22 +0300

    add/responsive and compatible with different screen sizes

--- src/app/components/Body/body-navbar/body-navbar.component.ts
+++ src/app/components/Body/body-navbar/body-navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-body-navbar',
   templateUrl: './body-navbar.component.html',
-  styleUrls: ['./body-navbar.component.css']
+  styleUrls: ['./body-navbar.component.css'],
 })
 export class BodyNavbarComponent {
-
+  showSidebar(e: Event) {
+    if (window.innerWidth > 767) return;
+    const target = e.target as HTMLDivElement;
+    target.classList.toggle('direction-sidebar');
+    const sidebar = document.getElementById('sidebar')
+      ?.parentElement as HTMLDivElement;
+    sidebar.classList.toggle('d-xs-none');
+  }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,14 +1,25 @@
-import { AfterContentInit, Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent implements AfterContentInit {
+export class SidebarComponent implements AfterViewInit {
   redBall: HTMLElement | any;
-  ngAfterContentInit(): void {
+  @Output() eventSendSidebar = new EventEmitter();
+  sidebar: HTMLElement | any;
+  ngAfterViewInit(): void {
     this.redBall = document.getElementById('redBall');
+    this.sidebar = document.getElementById('sidebar');
+    this.eventSendSidebar.emit(this.sidebar);
   }
   showRedBall(e: Event) {
     const target = e.target as HTMLImageElement;
